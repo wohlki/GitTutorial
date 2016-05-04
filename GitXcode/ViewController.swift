cf734e5668415005f88de52763dc97ecc770989f
Kerstin Kemmer <kerstin.kemmer@gmx.at>
2016-05-04 14:09:07 +0200

    Initial Commit

---
+++ GitXcode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitXcode
+//
+//  Created by Kerstin Kemmer on 04.05.16.
+//  Copyright © 2016 Stefan Wohlkinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
