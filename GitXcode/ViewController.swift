045232c4d3af04861c1536e7f31e2ce041b03572
Kerstin Kemmer <kerstin.kemmer@gmx.at>
2016-05-04 14:27:02 +0200

    initialize MapViewController

--- GitXcode/ViewController.swift
+++ GitXcode/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hallo world")
         print("Hallo")
+        let mapViewController = MapViewController()
+        print(mapViewController)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
