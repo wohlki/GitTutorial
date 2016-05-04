61e878593cab3ba88c09a20468188f56c0b28b75
Kerstin Kemmer <kerstin.kemmer@gmx.at>
2016-05-04 14:17:04 +0200

    Print Hello

--- GitXcode/ViewController.swift
+++ GitXcode/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hallo world")
+        print("Hallo")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
